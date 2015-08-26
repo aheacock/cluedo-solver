63a784dc3fd25dccaf597c60b25426416ffd87a2
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-08-26 02:11:41 +0200

    player model

---
+++ htdocs/model/player.php
@@ -0,0 +1,35 @@
+<?php
+
+  function create_player($player, $cards) {
+    $values["player"] = $player;
+    $values["cards"] = $cards;
+    $values["game"] = $_SESSION["game"];
+    return create_entry(
+      "player",
+      array("id", "cards", "game"),
+      array(),
+      $values
+    );
+  }
+
+  function select_player($player, $fields = array()) {
+    $player = select_entry(
+      "player",
+      array("id", "cards", "game"),
+      $budget,
+      $fields
+    );
+    return $player;
+  }
+
+  function select_players($criteria, $order_by = NULL, $ascending = true) {
+    return select_entries(
+      "player",
+      array("id", "cards", "game"),
+      array(),
+      array(),
+      $criteria,
+      $order_by,
+      $ascending
+    );
+  }
